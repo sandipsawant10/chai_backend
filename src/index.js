9b207b39c18987a9a7318568d252d4ba3150b906
sandipsawant10 <sandipsawant503@gmail.com>
2025-08-13 09:11:23 +0530

    add video & user model

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error", error);
+  });
+
+/*
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+})();
+
+*/
