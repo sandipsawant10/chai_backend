d9690ac9820ae5d6a5c582f15cc3e83ebdcb64b6
sandipsawant10 <sandipsawant503@gmail.com>
2025-08-20 22:28:06 +0530

    fix: correct route parameter casing for user channel profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -52,7 +52,7 @@ router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile);
 
 router.route("/history").get(verifyJWT, getWatchHistory);
 
